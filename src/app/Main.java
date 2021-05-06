e6cc077bfdf3b19c8c2ed7067644acc815a1990b
LargeWaffle <sirventhomas@gmail.com>
2021-05-06 13:46:06 +0200

    Layout main page

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,9 +2,13 @@ package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
@@ -18,11 +22,26 @@ public class Main extends Application {
 
         primaryStage.setTitle("IT Asset Manager");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.sizeToScene();
+
+        Vbox userFields = createContent();
 
         primaryStage.show();
     }
 
+    public static VBox createContent()
+    {
+        VBox vboxLayout = new VBox();
+        vboxLayout.setPadding(new Insets(10));
+        vboxLayout.setSpacing(8);
+
+        Text titleLabel = new Text("Welcome ! \n" + "Please login");
+        Text userLabel = new Text("Username :");
+        Text passwordLabel = new Text("Password :");
+
+        return vboxLayout;
+    }
 
     public static void main(String[] args) {
         launch(args);
