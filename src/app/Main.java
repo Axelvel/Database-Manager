9ecacbe93be8da76de0fd4513a70ccec53e2cb64
Sophie Teimournia <teimournia.sophie@laposte.net>
2021-05-13 18:15:16 +0200

    created database connection

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
@@ -15,7 +17,20 @@ public class Main extends Application {
         primaryStage.setTitle("IT Asset Manager");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-    }
+
+        Connection dbConnector = DriverManager.getConnection("jdbc:sqlite:C:/Users/teimo/Documents/COURS/INFO02/AP4B/Projet/Database-Manager/database/ap4b_db.db");
+
+        Statement stmt = dbConnector.createStatement();
+        ResultSet rs = stmt.executeQuery ("SELECT * FROM test_table");
+        while (rs.next()) {
+            System.out.println("ID : " + rs.getString("id"));
+            System.out.println("test : " + rs.getString("test_column"));
+        }
+
+
+        //String currentPath = new java.io.File(".").getCanonicalPath();
+        //System.out.println("Current dir:" + currentPath);
+        }
 
     public static void main(String[] args) {
         launch(args);
