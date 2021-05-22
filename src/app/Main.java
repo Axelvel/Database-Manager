71037616f0c562778a9d2345f98edae2db73f9fd
Axelvel <axelvel@live.fr>
2021-05-22 13:15:49 +0200

    :fire: Cleaned up Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -25,54 +25,9 @@ public class Main extends Application {
         primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.sizeToScene();
 
-        VBox userFields = createContent();
-
-        //User Database
-        User user1 = new User(1,"abc", "pass", "Jean", "Robert", true);
-        User user2 = new User(2,"xyz", "word", "Jeanne", "Roberta", true);
-
-        Users UserList = new Users();
-
-        UserList.addUser(user1);
-        UserList.addUser(user2);
-
-        UserList.showUserslist();
-
-        UserList.deleteUser(1);
-
-        UserList.showUserslist();
-
-        //Asset Database
-        Computer comp1 = new Computer(1, 1,"Computer1", true,true, "Window10", 1024, 16);
-        Asset asset1 = new Asset(2,1, "Asset2", true, false);
-
-        Database db = new Database();
-
-        db.addAsset(comp1);
-        db.addAsset(asset1);
-
-        db.showDatabase();
-
-        System.out.println(db.available());
-        System.out.println(db.status());
-
-
-
         primaryStage.show();
     }
 
-    public static VBox createContent()
-    {
-        VBox vboxLayout = new VBox();
-        vboxLayout.setPadding(new Insets(10));
-        vboxLayout.setSpacing(8);
-
-        Text titleLabel = new Text("Welcome ! \n" + "Please login");
-        Text userLabel = new Text("Username :");
-        Text passwordLabel = new Text("Password :");
-
-        return vboxLayout;
-    }
 
     public static void main(String[] args) {
         launch(args);
