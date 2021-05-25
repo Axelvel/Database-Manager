84b993e8919889702220f3c03bffbcd106157538
Sophie Teimournia <50867904+SophieShirine@users.noreply.github.com>
2021-05-25 13:04:57 +0200

    database implementation and tests

--- src/app/Main.java
+++ src/app/Main.java
@@ -37,16 +37,33 @@ public class Main extends Application {
 
     public static void main(String[] args) throws SQLException {
         /*DATABASE TEST*/
-        DatabaseConnection connection = new DatabaseConnection("database/ap4b_db.db");
+        /*DatabaseConnection connection = new DatabaseConnection("database/ap4b_db.db");
         connection.connect();
-        ResultSet rs = connection.query("SELECT * FROM test_table");
+        ResultSet rs = connection.query("SELECT * FROM assets_table");
 
         while (rs.next()) {
-            System.out.println("ID : " + rs.getString("id"));
-            System.out.println("test : " + rs.getString("test_column"));
+            System.out.println("code : " + rs.getString("asset_code"));
+            System.out.println("type : " + rs.getString("asset_type"));
+            System.out.println("status : " + rs.getString("asset_status"));
+            System.out.println("availability : " + rs.getString("asset_availability"));
         }
 
-        connection.close();
+        ResultSet rs_2 = connection.query("SELECT computers_table.computer_code, computers_table.computer_brand, computers_table.computer_os, computers_table.computer_memory, computers_table.computer_ram, assets_table.asset_status, assets_table.asset_availability\n" +
+                "FROM assets_table INNER JOIN computers_table ON assets_table.[asset_Code] = computers_table.[computer_Code];\n");
+        ResultSetMetaData rsmd = rs_2.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+
+        while (rs_2.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = rs_2.getString(i);
+                System.out.print(columnValue);
+            }
+            System.out.println("");
+        }
+
+
+        connection.close();*/
 
         launch(args);
     }
