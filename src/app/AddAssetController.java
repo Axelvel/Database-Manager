bc4c07e97e0c16525a9ab8f3fc5002a37e36940b
Sophie Teimournia <50867904+SophieShirine@users.noreply.github.com>
2021-05-25 21:50:45 +0200

    updating addAssetView to add computers

--- src/app/AddAssetController.java
+++ src/app/AddAssetController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.sql.ResultSet;
+
 public class AddAssetController extends Controller {
 
     public AddAssetController(Model dataModel) {
@@ -25,10 +27,31 @@ public class AddAssetController extends Controller {
     private RadioButton statusToggle2;
 
     @FXML
-    private RadioButton availabilityToggle1;
+    private RadioButton statusToggle3;
 
     @FXML
-    private RadioButton availabilityToggle2;
+    private RadioButton statusToggle4;
+
+    @FXML
+    private RadioButton osToggle1;
+
+    @FXML
+    private RadioButton osToggle2;
+
+    @FXML
+    private RadioButton osToggle3;
+
+    @FXML
+    private RadioButton typeComputer;
+
+    @FXML
+    private TextField brandTextField;
+
+    @FXML
+    private TextField memoryTextField;
+
+    @FXML
+    private TextField ramTextField;
 
 
     @FXML
@@ -41,39 +64,44 @@ public class AddAssetController extends Controller {
     @FXML
     public void addAsset() throws Exception {
 
-        String code;
-        String name;
-        String status;
-        boolean available;
+        //TODO : allow different type of assets (maybe divide this function into multiples ?)
+        //TODO : Create controls on every fields
 
-        int size;
-        size = dataModel.database.getDatabase().size();
-        code = dataModel.database.getDatabase().get(size - 1).getCode() + 1; //TODO : correct code incrementation
+        String status = null;
 
-        name = nameField.getText();
-
-        //TODO : correct status attribute
-        status = "Good";
         if (statusToggle1.isSelected()) {
-            //Status: No
-            //status = false;
-        } else {
-            //Status: Yes
-            //status = true;
+            status = "Excellent";
+        } else if (statusToggle2.isSelected()){
+            status = "Good";
+        } else if (statusToggle3.isSelected()) {
+            status = "Okay";
+        } else if (statusToggle4.isSelected()){
+            status = "Bad";
         }
 
-        if (availabilityToggle1.isSelected()) {
-            //Available : No
-            available = false;
-        } else {
-            //Available : Yes
-            available = true;
-        }
+        if(typeComputer.isSelected()){
 
-        //TODO : ALLOW DIFFERENT ASSETS TYPE
-        if (name != null) {
-            Asset asset = new Asset(code, Type.COMPUTER, status, available);
-            dataModel.database.addAsset(asset);
+            //count the number of computers in the database
+            String code;
+            ResultSet rs = dataModel.getDb().query("SELECT COUNT() FROM computers_table");
+            int nbComputers = rs.getInt(1) + 1;
+            code = "COMP" + nbComputers;
+
+            String os = null;
+            if(osToggle1.isSelected()){
+                os = "Windows";
+            }else if(osToggle2.isSelected()){
+                os = "Linux";
+            }else if(osToggle3.isSelected()){
+                os = "MacOS";
+            }
+
+
+            Computer c = new Computer(code,Type.COMPUTER,status,true,brandTextField.getText(),os,
+                    Integer.parseInt(memoryTextField.getText()),Integer.parseInt(ramTextField.getText()));
+
+            dataModel.database.addAsset(c);
+            dataModel.getDb().addAsset(c);
             goBack();
         }
 
