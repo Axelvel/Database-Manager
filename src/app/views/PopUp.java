b6c8b170f0bc0b4942dce4b4b084a8b341c60b33
Axelvel <axelvel@live.fr>
2021-06-19 18:04:04 +0200

    :bug: Fixed font rendering issues

--- src/app/views/PopUp.java
+++ src/app/views/PopUp.java
@@ -52,6 +52,8 @@ public class PopUp {
         Label message= new Label(text);
 
         Button closeButton= new Button("Close");
+        closeButton.setStyle("-fx-font-family: \"Verdana\"");
+
         closeButton.setOnAction(e -> popupwindow.close());
 
         VBox layout= new VBox(10);
@@ -72,70 +74,73 @@ public class PopUp {
         popupwindow.setTitle("ASSET DETAILS");
 
         Label title = new Label("ASSET INFORMATIONS");
-        title.setFont(new Font("Arial", 40));
+        title.setStyle("-fx-font-family: \"Verdana\"; -fx-font-size: 40");
+
+        //title.setFont(new Font("\"Arial\"", 40));
 
         TextFlow content = new TextFlow();
 
         Text codeTitle=new Text("Code : ");
-        codeTitle.setStyle("-fx-font-weight: bold");
+        codeTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold -fx ");
         Text code=new Text(asset.getCode()+"\n");
-        code.setStyle("-fx-font-weight: regular");
+        code.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         Text typeTitle=new Text("Type  : ");
-        typeTitle.setStyle("-fx-font-weight: bold");
+        typeTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text type=new Text(asset.getType()+"\n");
-        type.setStyle("-fx-font-weight: regular");
+        type.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         Text statusTitle=new Text("Status : ");
-        statusTitle.setStyle("-fx-font-weight: bold");
+        statusTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text status=new Text(asset.getStatus()+"\n");
-        status.setStyle("-fx-font-weight: regular");
+        status.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         Text availabilityTitle=new Text("Availability : ");
-        availabilityTitle.setStyle("-fx-font-weight: bold");
+        availabilityTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text availability=new Text(asset.isAvailable()+"\n");
-        availability.setStyle("-fx-font-weight: regular");
+        availability.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         Text brandTitle=new Text("Brand : ");
-        brandTitle.setStyle("-fx-font-weight: bold");
+        brandTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text brand=new Text(asset.getBrand()+"\n");
-        brand.setStyle("-fx-font-weight: regular");
+        brand.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         content.getChildren().addAll(codeTitle,code,typeTitle,type,statusTitle,status,availabilityTitle,availability,brandTitle,brand);
 
         if(asset.getType() == Type.COMPUTER){
             Text osTitle=new Text("Os : ");
-            osTitle.setStyle("-fx-font-weight: bold");
+            osTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
             Text os=new Text(asset.getOs()+"\n");
-            os.setStyle("-fx-font-weight: regular");
+            os.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
             Text memoryTitle=new Text("Memory : ");
-            memoryTitle.setStyle("-fx-font-weight: bold");
+            memoryTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
             Text memory=new Text(asset.getMemory()+"\n");
-            memory.setStyle("-fx-font-weight: regular");
+            memory.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
             Text ramTitle=new Text("Ram : ");
-            ramTitle.setStyle("-fx-font-weight: bold");
+            ramTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
             Text ram=new Text(asset.getRam()+"\n");
-            ram.setStyle("-fx-font-weight: regular");
+            ram.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
             content.getChildren().addAll(osTitle,os,memoryTitle,memory,ramTitle,ram);
         }else if(asset.getType() == Type.KEYBOARD){
             Text wirelessTitle=new Text("Wireless : ");
-            wirelessTitle.setStyle("-fx-font-weight: bold");
+            wirelessTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
             Text wireless=new Text(asset.getWireless()+"\n");
-            wireless.setStyle("-fx-font-weight: regular");
+            wireless.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
             Text switchesTitle=new Text("Switches : ");
-            switchesTitle.setStyle("-fx-font-weight: bold");
+            switchesTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
             Text switches=new Text(asset.getSwitches()+"\n");
-            switches.setStyle("-fx-font-weight: regular");
+            switches.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
             content.getChildren().addAll(wirelessTitle,wireless,switchesTitle,switches);
         }
 
         content.setTextAlignment(TextAlignment.CENTER);
         Button closeButton= new Button("Close");
+        closeButton.setStyle("-fx-font-family: \"Verdana\"");
         closeButton.setOnAction(e -> popupwindow.close());
 
         VBox layout= new VBox(10);
@@ -167,23 +172,24 @@ public class PopUp {
         }
 
         Text usernameTitle=new Text("\nUsername : ");
-        usernameTitle.setStyle("-fx-font-weight: bold");
+        usernameTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text username=new Text(user.getUsername()+"\n");
-        username.setStyle("-fx-font-weight: regular");
+        username.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         Text nameTitle=new Text("Last name  : ");
-        nameTitle.setStyle("-fx-font-weight: bold");
+        nameTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text name=new Text(user.getLastName()+"\n");
-        name.setStyle("-fx-font-weight: regular");
+        name.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         Text surnameTitle=new Text("Surname  : ");
-        surnameTitle.setStyle("-fx-font-weight: bold");
+        surnameTitle.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: bold");
         Text surname=new Text(user.getName()+"\n");
-        surname.setStyle("-fx-font-weight: regular");
+        surname.setStyle("-fx-font-family: \"Verdana\"; -fx-font-weight: regular");
 
         content.getChildren().addAll(usernameTitle,username,nameTitle,name,surnameTitle,surname);
         content.setTextAlignment(TextAlignment.CENTER);
         Button closeButton= new Button("Close");
+        closeButton.setStyle("-fx-font-family: \"Verdana\"");
         closeButton.setOnAction(e -> popupwindow.close());
 
         VBox layout= new VBox(10);
